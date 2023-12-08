edd33d8771b2f24ec4b9f134f04f5fc10f625b63
silashorta <92862835+silashorta@users.noreply.github.com>
2023-12-08 15:21:18 -0300

    update icone pdf

--- components/About/DownloadButton/DownloadButton.jsx
+++ components/About/DownloadButton/DownloadButton.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { saveAs } from 'file-saver';
 import styles from './DownloadButton.module.css'
+import iconePDF from './download-direto.png'
+import iconeClicado from './icone-clicado.png'
+
 
 const DownloadButton = () => {
-  const handleDownload = () => {
-    const pdfUrl = '../../assets/files/Curriculo.pdf';
+  const [isHovered, setIsHovered] = useState(false);
 
+
+  const handleDownload = () => {
+    
+    const pdfUrl = '../../assets/files/Curriculo.pdf';
+    
     // Use a função saveAs para iniciar o download do arquivo
     saveAs(pdfUrl, 'Curriculo.pdf');
   };
 
+
   return (
-    <button data-aos="flip-up" className={styles.downloadButton} onClick={handleDownload}>
-      Download CV
-    </button>
+    <div>
+      <button
+        data-aos="flip-up"
+        className={styles.downloadButton}
+        onClick={handleDownload}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        Download CV
+
+        <img src={isHovered ? iconeClicado : iconePDF}
+          alt=""
+          className={styles.pdfIcon} />
+      </button>
+    </div>
   );
 };
 
