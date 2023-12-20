36dd350dddbdd4c36a782c7278c23d85ec62f732
silashorta <92862835+silashorta@users.noreply.github.com>
2023-12-20 20:19:52 -0300

    up hero

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
         <h2 className={styles.subtitulo} data-aos="fade">Eu sou o Silas Horta <div className={`animate__animated animate__pulse ${styles.text_main}`} >Web Developer</div> &lt;/&gt;</h2>
       </div>
       <div >
-        <img src="../../assets/images/teste.png" alt="" className={"animate__animated animate__backInUp " + styles.hero__img}/>
+        <img src="../../assets/images/eu2.png" alt="" className={"animate__animated animate__backInUp " + styles.hero__img}/>
       </div>
     </div>
   )
