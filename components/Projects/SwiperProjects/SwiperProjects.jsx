6dbef48c255c64a64052c7b12424a297f9b6be0e
silashorta <92862835+silashorta@users.noreply.github.com>
2023-12-20 21:53:26 -0300

    Update SwiperProjects.jsx

--- components/Projects/SwiperProjects/SwiperProjects.jsx
+++ components/Projects/SwiperProjects/SwiperProjects.jsx
@@ -38,7 +38,7 @@ export default function SwiperProjects() {
       titulo: "Portfólio",
       imageUrl: "portfolio.png",
       descricao: `O projeto é um portfólio web desenvolvido em React. Utilizo diversas dependências para aprimorar a experiência do usuário, incluindo bibliotecas como "@ramonak/react-progress-bar" para barras de progresso, "animate.css" para animações e "swiper" para carrosséis interativos. A integração de "nodemailer" facilita o envio de e-mails, "express" e "cors" são empregados para o backend, e "formik" gerencia formulários. O uso de "react-intersection-observer" e "react-visibility-sensor" lida com a visibilidade de elementos.`,
-      siteUrl: "https://portfolio-project-fxes6cdti-silashorta.vercel.app/",
+      siteUrl: "https://silashortaportfolio.vercel.app/",
       projetoUrl: "https://github.com/silashorta/portfolio-project",
       icons: ["biblioteca.png", "bootstrap.png", "node-js.png", "express.png", "git.png"]
     }
